122692dfe3a2981839630ce1406f1fbdb1acbca2
Derek Illchuk <dillchuk@gmail.com>
2015-05-02 15:06:53 -0500

    Initial commit.

---
+++ Module.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace InvalidSession;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+use Zend\Session\Container;
+use Zend\Session\AbstractManager;
+
+class Module {
+
+    public function getConfig() {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function onBootstrap(MvcEvent $e) {
+        $eventManager = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+
+        $sessionManager = $e->getApplication()->getServiceManager()
+        ->get('Zend\Session\SessionManager');
+        $this->forgetInvalidSession($sessionManager);
+    }
+
+    protected function forgetInvalidSession(AbstractManager $sessionManager) {
+        try {
+            $sessionManager->start();
+            Container::setDefaultManager($sessionManager);
+            return;
+        } catch (\Exception $e) {
+
+        }
+        /**
+         * Session validation failed: toast it and carry on.
+         */
+        // @codeCoverageIgnoreStart
+        session_unset();
+        // @codeCoverageIgnoreEnd
+    }
+
+}
